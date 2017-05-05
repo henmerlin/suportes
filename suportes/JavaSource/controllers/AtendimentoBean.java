5ef5121161b1bca6b2b45d70ea5565bd2f7e98d3
henmerlin <henmerlin@gmail.com>
2017-05-05 12:27:05 -0300

    Alterações nos campos, close #1.

--- suportes/JavaSource/controllers/AtendimentoBean.java
+++ suportes/JavaSource/controllers/AtendimentoBean.java
@@ -1,56 +1,60 @@
 package controllers;
 
+import entidades.Atendimento;
 import java.util.List;
-
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
-import entidades.Atendimento;
+import javax.faces.model.SelectItem;
 import model.AtendimentoServico;
 import util.JSFUtil;
 
 @ViewScoped
-@ManagedBean(name="atendimentoBean")
-public class AtendimentoBean{
+@ManagedBean(name = "atendimentoBean")
+public class AtendimentoBean {
 
-	private Atendimento atendimento;
+    private Atendimento atendimento;
 
-	@EJB
-	private AtendimentoServico servicoAtendimento;
+    @EJB
+    private AtendimentoServico servicoAtendimento;
 
-	public AtendimentoBean() {
-		this.atendimento = new Atendimento();
-	}
+    public AtendimentoBean() {
+        this.atendimento = new Atendimento();
+    }
 
-	public Atendimento getAtendimento() {
-		return atendimento;
-	}
+    public SelectItem[] getMyBooleanValues() {
+        return new SelectItem[]{
+            new SelectItem(Boolean.TRUE, "Sim"),
+            new SelectItem(Boolean.FALSE, "Não")
+        };
+    }
 
-	public void setAtendimento(Atendimento atendimento) {
-		this.atendimento = atendimento;
-	}
+    public Atendimento getAtendimento() {
+        return atendimento;
+    }
 
-	public Atendimento finalizar(){
-		
-		try {
-			
-			this.servicoAtendimento.salvar(this.atendimento);
-			JSFUtil.addInfoMessage("Atendimento encerrado!");
-			this.atendimento = new Atendimento();
-			
-		} catch (Exception e) {
-			JSFUtil.addErrorMessage(e.getMessage());
-		}
-	
-		
-		return new Atendimento();
-	}
-	
-	public List<Atendimento> listar(){
-		
-		return this.servicoAtendimento.listar();
-	}	
+    public void setAtendimento(Atendimento atendimento) {
+        this.atendimento = atendimento;
+    }
 
+    public Atendimento finalizar() {
+
+        try {
+
+            this.servicoAtendimento.salvar(this.atendimento);
+            JSFUtil.addInfoMessage("Atendimento encerrado!");
+            this.atendimento = new Atendimento();
+
+        } catch (Exception e) {
+            JSFUtil.addErrorMessage(e.getMessage());
+        }
+
+        return new Atendimento();
+    }
+
+    public List<Atendimento> listar() {
+
+        return this.servicoAtendimento.listar();
+    }
 
 }
--- suportes/JavaSource/entidades/Atendimento.java
+++ suportes/JavaSource/entidades/Atendimento.java
@@ -7,195 +7,192 @@ import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
-
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Atendimento {
 
-	@Id
-	@GeneratedValue
-	private Integer Id;
-	
-	private String tecnico;
+    @Id
+    @GeneratedValue
+    private Integer Id;
 
-	@NotEmpty
-	@Length(min=8, max=10, message="Padrão: G00XXXXX.")
-	private String colaborador;
-	
-	@NotEmpty
-	private String acompanhamento;
-	
-	@NotEmpty
-	private String instancia;
-	
-	@NotEmpty
-	private String ordem;
-	
-	@NotEmpty
-	private String servico;
+    private String tecnico;
 
-	@OneToOne
-	@NotNull
-	private Motivo motivo;
+    @NotEmpty
+    @Length(min = 8, max = 10)
+    private String colaborador;
 
-	@Lob
-	private String observacao;
+    @NotEmpty
+    private String acompanhamento;
 
-	private Date data;
-	
-	private Boolean acesso;
-	
-	private Boolean ferramentas;
-	
-	private Boolean demandaExterna;
+    @NotEmpty
+    private String instancia;
 
+    @NotEmpty
+    private String ordem;
 
-	public Atendimento() {
-	
-	}
+    @NotEmpty
+    private String servico;
 
+    @OneToOne
+    @NotNull
+    private Motivo motivo;
 
-	public Boolean getDemandaExterna() {
-		return demandaExterna;
-	}
+    @Lob
+    private String observacao;
 
+    private Date data;
 
-	public void setDemandaExterna(Boolean demandaExterna) {
-		this.demandaExterna = demandaExterna;
-	}
+    @NotNull
+    private Boolean acesso;
 
+    @NotNull
+    private Boolean ferramentas;
 
-	public String getServico() {
-		return servico;
-	}
+    @NotNull
+    private Boolean demandaExterna;
 
-	public void setServico(String servico) {
-		this.servico = servico;
-	}
+    public Atendimento() {
+    }
 
-	public String getAcompanhamento() {
-		return acompanhamento;
-	}
+    public Boolean getDemandaExterna() {
+        return demandaExterna;
+    }
 
-	public void setAcompanhamento(String acompanhamento) {
-		this.acompanhamento = acompanhamento;
-	}
+    public void setDemandaExterna(Boolean demandaExterna) {
+        this.demandaExterna = demandaExterna;
+    }
 
+    public String getServico() {
+        return servico;
+    }
 
-	public String getInstancia() {
-		return instancia;
-	}
+    public void setServico(String servico) {
+        this.servico = servico;
+    }
 
-	public void setInstancia(String instancia) {
-		this.instancia = instancia;
-	}
+    public String getAcompanhamento() {
+        return acompanhamento;
+    }
 
-	public String getOrdem() {
-		return ordem;
-	}
+    public void setAcompanhamento(String acompanhamento) {
+        this.acompanhamento = acompanhamento;
+    }
 
-	public void setOrdem(String ordem) {
-		this.ordem = ordem;
-	}
+    public String getInstancia() {
+        return instancia;
+    }
 
-	public String getObservacao() {
-		return observacao;
-	}
+    public void setInstancia(String instancia) {
+        this.instancia = instancia;
+    }
 
-	public void setObservacao(String observacao) {
-		this.observacao = observacao;
-	}
+    public String getOrdem() {
+        return ordem;
+    }
 
-	public Motivo getMotivo() {
-		return motivo;
-	}
+    public void setOrdem(String ordem) {
+        this.ordem = ordem;
+    }
 
-	public void setMotivo(Motivo motivo) {
-		this.motivo = motivo;
-	}
+    public String getObservacao() {
+        return observacao;
+    }
 
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
 
-	public String getColaborador() {
-		return colaborador;
-	}
+    public Motivo getMotivo() {
+        return motivo;
+    }
 
-	public void setColaborador(String colaborador) {
-		this.colaborador = colaborador;
-	}
+    public void setMotivo(Motivo motivo) {
+        this.motivo = motivo;
+    }
 
-	public Integer getId() {
-		return Id;
-	}
+    public String getColaborador() {
+        return colaborador;
+    }
 
-	public void setId(Integer id) {
-		Id = id;
-	}
+    public void setColaborador(String colaborador) {
+        this.colaborador = colaborador;
+    }
 
-	public String getTecnico() {
-		return tecnico;
-	}
+    public Integer getId() {
+        return Id;
+    }
 
-	public void setTecnico(String tecnico) {
-		this.tecnico = tecnico;
-	}
+    public void setId(Integer id) {
+        Id = id;
+    }
 
+    public String getTecnico() {
+        return tecnico;
+    }
 
-	public Date getData() {
-		return data;
-	}
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
 
+    public Date getData() {
+        return data;
+    }
 
-	public void setData(Date data) {
-		this.data = data;
-	}
+    public void setData(Date data) {
+        this.data = data;
+    }
 
-	public Boolean getAcesso() {
-		return acesso;
-	}
+    public Boolean getAcesso() {
+        return acesso;
+    }
 
-	public void setAcesso(Boolean acesso) {
-		this.acesso = acesso;
-	}
+    public void setAcesso(Boolean acesso) {
+        this.acesso = acesso;
+    }
 
-	public Boolean getFerramentas() {
-		return ferramentas;
-	}
+    public Boolean getFerramentas() {
+        return ferramentas;
+    }
 
-	public void setFerramentas(Boolean ferramentas) {
-		this.ferramentas = ferramentas;
-	}
+    public void setFerramentas(Boolean ferramentas) {
+        this.ferramentas = ferramentas;
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+        return result;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
-		return result;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Atendimento other = (Atendimento) obj;
+        if (Id == null) {
+            if (other.Id != null) {
+                return false;
+            }
+        } else if (!Id.equals(other.Id)) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Atendimento other = (Atendimento) obj;
-		if (Id == null) {
-			if (other.Id != null)
-				return false;
-		} else if (!Id.equals(other.Id))
-			return false;
-		return true;
-	}
+    @Override
+    public String toString() {
+        return "Atendimento [Id=" + Id + "]";
+    }
 
-	@Override
-	public String toString() {
-		return "Atendimento [Id=" + Id + "]";
-	}
-	
 }
